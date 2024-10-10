f0f1f0ca8ddc96853a2a62646e193b59cfa640a5
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-10-10 18:20:04 +0530

    Day3 part 2 also done

--- day3.rs
+++ day3.rs
@@ -20,13 +20,17 @@ fn main() {
 
     let mut curr = lines.next().unwrap().unwrap();
     let mut prev = ".".repeat(curr.len());
+
     let mut is_done = false;
-    let mut sum = 0;
+    let (mut sum1, mut sum2) = (0, 0);
 
     while !is_done {
         if let Some(Ok(next)) = lines.next() {
-            let res = part1(&prev, &curr, &next);
-            sum += res;
+            let res1 = part1(&prev, &curr, &next);
+            let res2 = part2(&prev, &curr, &next);
+
+            sum1 += res1;
+            sum2 += res2;
 
             prev = curr.to_string();
             curr = next;
@@ -34,18 +38,98 @@ fn main() {
             is_done = true;
             let next = ".".repeat(curr.len());
 
-            let res = part1(&prev, &curr, &next);
-            sum += res;
+            let res1 = part1(&prev, &curr, &next);
+            let res2 = part2(&prev, &curr, &next);
+
+            sum1 += res1;
+            sum2 += res2;
         }
     }
 
-    println!("Result: {}", sum);
+    println!("Result 1: {}", sum1);
+    println!("Result 2: {}", sum2);
 }
 
 fn _dump(m: &Vec<Vec<char>>) {
     for m in m {
         println!("{m:?}");
     }
+}
+
+fn part2(prev: &str, curr: &str, next: &str) -> usize {
+    let mut sum = 0;
+
+    let mt = vec![
+        prev.chars().into_iter().collect::<Vec<char>>(),
+        curr.chars().into_iter().collect::<Vec<char>>(),
+        next.chars().into_iter().collect::<Vec<char>>(),
+    ];
+
+    for c in 0..mt[1].len() {
+        if mt[1][c] == '*' {
+            let res = dfs(&mt, (1, c));
+
+            if res.len() == 2 {
+                // println!("{res:?}");
+                sum += res[0].parse::<usize>().unwrap() * res[1].parse::<usize>().unwrap();
+            }
+        }
+    }
+    sum
+}
+
+fn dfs(mt: &Vec<Vec<char>>, r: (usize, usize)) -> Vec<String> {
+    // _dump(mt);
+
+    let mut gg: Vec<String> = vec![];
+
+    let (rc, cc) = (3, mt[0].len() as i32);
+
+    let mut v = HashSet::new();
+
+    for inr in vec![-1, 0, 1] {
+        for inc in vec![-1, 0, 1] {
+            let (r, c) = r;
+            let nr = inr + r as i32;
+            let nc = inc + c as i32;
+            if nr >= 0 && nr < rc && nc >= 0 && nc < cc {
+                let mut res = String::new();
+                let nr = nr as usize;
+                let nc = nc as usize;
+
+                if v.contains(&(nr, nc)) {
+                    continue;
+                }
+
+                if mt[nr][nc].is_digit(10) {
+                    for i in (0..nc).rev() {
+                        let ch = mt[nr][i];
+
+                        if ch.is_digit(10) {
+                            v.insert((nr, i));
+                            res.insert(0, ch);
+                        } else {
+                            break;
+                        }
+                    }
+                    for i in nc..cc as usize {
+                        let ch = mt[nr][i];
+
+                        if ch.is_digit(10) {
+                            v.insert((nr, i));
+                            res.push(ch);
+                        } else {
+                            break;
+                        }
+                    }
+
+                    gg.push(res);
+                }
+            }
+        }
+    }
+
+    gg
 }
 
 fn part1(prev: &str, curr: &str, next: &str) -> usize {
