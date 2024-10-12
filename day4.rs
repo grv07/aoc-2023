8274bcab9825a5581f6a7a876087b43a6b59f0a0
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-10-12 21:45:49 +0530

    Done part2 and restructure the code to make it more maintainable and readable

--- day4.rs
+++ day4.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Lines;
 use std::io::{BufRead, BufReader};
@@ -13,50 +14,86 @@ fn get_lines() -> Lines<BufReader<File>> {
 }
 
 fn main() {
-    let lines = get_lines();
-    let sum = part1(lines);
-    println!("Part 1: {sum}");
-}
+    let mut lines = get_lines();
 
-fn part1(mut lines: Lines<BufReader<File>>) -> i32 {
-    let mut sum = 0;
+    let mut sum1 = 0;
+    let mut rs: HashMap<i32, i32> = HashMap::new();
 
     while let Some(Ok(line)) = lines.next() {
         if line.is_empty() {
             continue;
         }
-
-        let (_card, numbers) = line.split_once(':').unwrap();
-        let numbers = numbers.trim_start();
-
-        let (win, yh) = numbers.split_once('|').unwrap();
-        let win = win
-            .trim()
-            .split(' ')
-            .filter(|v| !v.is_empty())
-            .map(|v| v.trim().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-        let yh = yh
-            .trim()
-            .split(' ')
-            .filter(|v| !v.is_empty())
-            .map(|v| v.trim().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-
-        let winning_sum = |win: Vec<i32>, yh: Vec<i32>| -> i32 {
-            let mut ws: i32 = 0;
-
-            for w in win {
-                if yh.contains(&w) {
-                    ws += 1;
-                }
-            }
-
-            2_i32.pow(ws as u32) / 2
-        };
-
-        sum += winning_sum(win, yh);
+        sum1 += part1(&line);
+        part2(&line, &mut rs);
     }
+
+    println!("Part 1: {sum1}");
+
+    let sum2 = rs.values().sum::<i32>();
+    println!("Part 2: {sum2}");
+}
+
+fn parse_input(line: &str) -> (i32, Vec<i32>, Vec<i32>) {
+    let (card, numbers) = line.split_once(':').unwrap();
+    let card = card.replace("Card", "");
+    let card = card.trim();
+    let numbers = numbers.trim_start();
+
+    let (win, yh) = numbers.split_once('|').unwrap();
+    let win = win
+        .trim()
+        .split(' ')
+        .filter(|v| !v.is_empty())
+        .map(|v| v.trim().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    let yh = yh
+        .trim()
+        .split(' ')
+        .filter(|v| !v.is_empty())
+        .map(|v| v.trim().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    (card.parse::<i32>().unwrap(), win, yh)
+}
+
+fn get_win_count(win: Vec<i32>, yh: Vec<i32>) -> i32 {
+    let mut count: i32 = 0;
+
+    for w in win {
+        if yh.contains(&w) {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn part2(line: &str, rs: &mut HashMap<i32, i32>) {
+    let (card, win, yh) = parse_input(line);
+    rs.entry(card).or_insert(1);
+
+    let wc = get_win_count(win, yh);
+
+    let tc = rs.get(&card).unwrap().clone();
+
+    for i in 1..wc + 1 {
+        rs.entry(card + i)
+            .and_modify(|v| {
+                // for j in 0..tc {
+                *v += tc;
+                // }
+            })
+            .or_insert(1 + tc);
+    }
+    // println!("line: {card} {:?}", wc);
+}
+
+fn part1(line: &str) -> i32 {
+    let mut sum = 0;
+
+    let (_, win, yh) = parse_input(&line);
+
+    let wc = get_win_count(win, yh);
+    sum += 2_i32.pow(wc as u32) / 2;
 
     sum
 }
