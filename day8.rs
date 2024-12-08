bd4560cb3307facec4265cc598db52c2e33fa160
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-12-08 13:09:09 +0530

    Day 8 part 1 is solution

---
+++ day8.rs
@@ -0,0 +1,60 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn parse_input() -> (String, HashMap<String, (String, String)>) {
+    let mut args = std::env::args();
+    args.next();
+
+    let file_name = args.next().unwrap();
+
+    let br = BufReader::new(File::open(file_name).unwrap());
+
+    let mut lines = br.lines();
+    let steps = lines.next().unwrap().unwrap().to_string();
+    // println!("{steps}");
+
+    let mut res = HashMap::new();
+    for line in lines {
+        let line = line.unwrap();
+        if line.is_empty() {
+            continue;
+        }
+        let (node, path) = line.split_once('=').unwrap();
+        let (path1, path2) = path.split_once(',').unwrap();
+        let path1 = path1.trim().replace("(", "").to_string();
+        let path2 = path2.trim().replace(")", "").to_string();
+
+        res.insert(node.trim().to_string(), (path1, path2));
+    }
+
+    (steps, res)
+}
+
+fn walk(steps: &str, nodes: &HashMap<String, (String, String)>) {
+    let mut start = "AAA";
+    let mut count = 0;
+    for step in steps.chars().cycle() {
+        if let Some(node) = nodes.get(start) {
+            count += 1;
+            if step == 'L' {
+                start = &node.0;
+            } else {
+                start = &node.1;
+            }
+
+            if start == "ZZZ" {
+                println!("COUNT: {count}");
+                return;
+            }
+
+            // println!("{start}{step}");
+        }
+    }
+}
+
+fn main() {
+    let (input, map) = parse_input();
+
+    walk(&input, &map);
+}
